fa0ee428eadd6050ac6102072a5b76710a2a4547
mukhammadqodir <98277057+mukhammadqodir-alijonov@users.noreply.github.com>
2023-02-15 16:10:01 +0500

    Update config.py

--- insta/data/config.py
+++ insta/data/config.py
@@ -5,6 +5,6 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "5770566936:AAG3fee7fKkapfPZB9Hx7CVhkscnBgO0YUY"  # Bot toekn
+BOT_TOKEN = ${{secrets.TOKEN}}  # Bot toekn
 ADMINS = [1755665750]  # adminlar ro'yxati
 
